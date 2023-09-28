872991e5600e24d5a5c229216ee2962403f320b6
Lars Hartvigsen <lhartvik@gmail.com>
2023-09-28 12:45:19 +0200

    init

---
+++ src/test/java/no/test/ConnectTest.java
@@ -0,0 +1,43 @@
+package no.test;
+
+import org.junit.jupiter.api.Test;
+
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.nio.charset.StandardCharsets;
+import java.time.LocalDateTime;
+import java.util.List;
+
+class ConnectTest {
+
+    @Test
+    void foo() throws com.jcraft.jsch.JSchException, com.jcraft.jsch.SftpException {
+        System.out.println("LocalDateTime.now() = " + LocalDateTime.now());
+
+        com.jcraft.jsch.JSch javaSecureChannel = new com.jcraft.jsch.JSch();
+
+        String prvKey = "...";
+
+        byte[] bytes = prvKey.getBytes();
+        javaSecureChannel.addIdentity("histark_import", bytes, null, null);
+
+        com.jcraft.jsch.Session sftpSession = javaSecureChannel.getSession("histark_import", "sftp-histark.preprod.local", 22);
+
+        sftpSession.setConfig("PreferredAuthentications", "publickey");
+        sftpSession.setConfig("StrictHostKeyChecking", "no");
+        sftpSession.connect();
+
+        com.jcraft.jsch.ChannelSftp sftpChannel = (com.jcraft.jsch.ChannelSftp) sftpSession.openChannel("sftp");
+        sftpChannel.connect();
+
+        InputStream inputStream = sftpChannel.get("/inbound/OS.INPUT");
+        InputStreamReader inputStreamReader = new InputStreamReader(inputStream, StandardCharsets.ISO_8859_1);
+        BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
+
+        List<String> strings = bufferedReader.lines().toList();
+
+        System.out.println("strings = " + strings);
+
+    }
+}
